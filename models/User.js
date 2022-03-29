956e09b8d94792ba6b85fff98c6b67bb0deaad8b
robbiemurphy <rmurphy987@gmail.com>
2022-03-28 20:02:16 -0500

    feature/db

--- models/User.js
+++ models/User.js
@@ -1,49 +1,47 @@
 User.init(
-    {
-      
-      id: {
-        
-        type: DataTypes.INTEGER,
-        
-        allowNull: false,
-        
-        primaryKey: true,
-        
-        autoIncrement: true
-      },
-      
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        
-        unique: true,
-        
-        validate: {
-          isEmail: true
-        }
-      },
-      
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          
-          len: [4]
-        }
+  {
+    // define an id column
+    id: {
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+    },
+    // define a username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // define an email column
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // there cannot be any duplicate email values in this table
+      unique: true,
+      // if allowNull is set to false, we can run our data through validators before creating the table data
+      validate: {
+        isEmail: true
       }
     },
-    {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user'
+    // define a password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // this means the password must be at least four characters long
+        len: [4]
+      }
     }
-  );
-
-module.exports = User;
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
