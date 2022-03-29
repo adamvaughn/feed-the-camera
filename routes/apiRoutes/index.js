11b93459394ee0ae8b192886642ca90a33fe229f
robbiemurphy <rmurphy987@gmail.com>
2022-03-28 19:42:08 -0500

    routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,10 +3,18 @@ const blogRoutes = require('../apiRoutes/blogRoutes');
 const editorRoutes = require('../apiRoutes/editorRoutes');
 const firebaseRoutes = require('../apiRoutes/firebaseRoutes');
 const homeRoutes = require('../apiRoutes/homeRoutes');
+// new stuff
+const userRoutes = require('../apiRoutes/userRoutes');
+const apiRoutes = require('./api');
 
 router.use(blogRoutes);
 router.use(editorRoutes);
 router.use(firebaseRoutes);
 router.use(homeRoutes);
+// new stuff
+router.use('users', userRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;
